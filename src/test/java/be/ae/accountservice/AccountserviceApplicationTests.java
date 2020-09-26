405287b92c68328d346bcf9d4be244caa119a7f0
sstoelen <sebastian.stoelen@ae.be>
2020-09-26 17:01:17 +0200

    Milestone 1 from spring starter

---
+++ src/test/java/be/ae/accountservice/AccountserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ae.accountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
